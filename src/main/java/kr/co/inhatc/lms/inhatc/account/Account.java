97b82427e7cbb7e6f4392536e2780a2e9064e552
Hwan <seunghwan546@naver.com>
2020-08-28 01:23:46 +0900

    initial commit

---
+++ src/main/java/kr/co/inhatc/lms/inhatc/account/Account.java
@@ -0,0 +1,27 @@
+package kr.co.inhatc.lms.inhatc.account;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity @Getter @Setter @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+}
---
+++ src/main/java/kr/co/inhatc/lms/inhatc/securityConfig/SecurityConfig.java
@@ -0,0 +1,18 @@
+package kr.co.inhatc.lms.inhatc.securityConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration @EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token",
+                "/email-login", "/login-by-email").permitAll()
+                .anyRequest().authenticated();
+    }
+}
