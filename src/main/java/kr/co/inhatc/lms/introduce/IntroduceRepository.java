9906cb93893e0fa5648388a1477dda18130f98d8
Hwan <seunghwan546@naver.com>
2020-10-06 19:23:22 +0900

    1006 강의소개 제작 완료 TODO 수정기능

--- src/main/java/kr/co/inhatc/lms/introduce/IntroduceRepository.java
+++
@@ -1,9 +0,0 @@
-//package kr.co.inhatc.lms.introduce;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Transactional
-//public interface IntroduceRepository extends JpaRepository<Introduce, Long> {
-//}
-////
